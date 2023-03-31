bb14bf6cddb053e941559715259ba5484165a3f9
HackDev <80620538+Hackx2@users.noreply.github.com>
2023-03-31 02:27:20 +0100

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -88,7 +88,8 @@ class Main extends Sprite
 		}
 	
 		ClientPrefs.loadDefaultKeys();
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen));
+		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
+
 		#if !mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);

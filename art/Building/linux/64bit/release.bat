1ca2cafd336303060b9bc5debb87dddae5e12dba
HackDev <80620538+Hackx2@users.noreply.github.com>
2024-04-10 13:14:42 +0100

    thats crazy

--- art/Building/linux/64bit/release.bat
+++ art/Building/linux/64bit/release.bat
@@ -2,9 +2,9 @@
 color 0a
 cd ../../../../
 echo BUILDING GAME
-haxelib run lime build windows -release
+haxelib run lime build Project.xml linux -release
 echo.
 echo done.
 pause
 pwd
-explorer.exe export\release\windows\bin
+explorer.exe export\release\linux\bin

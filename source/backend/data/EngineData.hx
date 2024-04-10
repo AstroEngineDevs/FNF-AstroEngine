7c68304a129ea0ab3201beb8f8d50778ead47870
HackDev <80620538+Hackx2@users.noreply.github.com>
2024-04-10 16:59:00 +0100

    damn

--- source/backend/data/EngineData.hx
+++ source/backend/data/EngineData.hx
@@ -1,6 +1,8 @@
 package backend.data;
 
+import haxegithub.utils.Repository;
 import flixel.FlxSprite;
+
 using StringTools;
 
 @:final class EngineData extends FlxSprite
@@ -15,12 +17,14 @@ using StringTools;
 		coreVersion: '0.2.2',
 		colorMenuImage: 0xff525252,
 		mainRepo: "https://github.com/Hackx2/FNF-AstroEngine",
+		commits: Repository.get('hackx2', 'FNF-AstroEngine/commits'),
 	}
 
 	//** ignore this **/
 	public function new()
 	{
         super();
+
 		if (backend.utils.ClientPrefs.data.lowQuality)
 			backend.utils.ClientPrefs.data.mouseEvents = false;
 	}
--- source/game/FPS.hx
+++ source/game/FPS.hx
@@ -84,13 +84,14 @@ class FPS extends TextField
 		{
 			text = "FPS: " + currentFPS;
 			var memoryMegas:Float = 0;
-			
+		
 			#if openfl
 			memoryMegas = Math.abs(FlxMath.roundDecimal(System.totalMemory / 1000000, 1));
 			text += "\nMemory: " + memoryMegas + " MB";
 			#end
 
             text += '\nAstro Engine: '+EngineData.mainCoreShit.coreVersion;
+
 			//text += '\nGit: '+Git.commitNumber;
 
 			textColor = 0xFFFFFFFF;

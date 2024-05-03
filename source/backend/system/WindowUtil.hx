1c276b8b4ba0cbc8cea718ba0dd01545c8733e8f
HackDev <80620538+Hackx2@users.noreply.github.com>
2024-05-03 21:43:54 +0100

    v1 maybe

--- source/backend/system/WindowUtil.hx
+++ source/backend/system/WindowUtil.hx
@@ -1,41 +1,41 @@
-// package backend.system;
+package backend.system;
 
-// import lime.app.Application;
+import lime.app.Application;
 
-// @:buildXml('
-// <target id="haxe">
-//     <lib name="dwmapi.lib" if="windows" />
-// </target>
-// ')
-// @:cppFileCode('
-//  #include <dwmapi.h>
-//  ')
-// #if windows
-// class WindowUtil
-// {
-// 	@:functionCode('
-//     int darkMode = enable ? 1 : 0;
-//     HWND window = GetActiveWindow();
-//     if (S_OK != DwmSetWindowAttribute(window, 19, &darkMode, sizeof(darkMode)))
-//         DwmSetWindowAttribute(window, 20, &darkMode, sizeof(darkMode));
-//     ')
-// 	public static function darkMode(enable:Bool)
-// 	{
-// 	}
+@:buildXml('
+<target id="haxe">
+    <lib name="dwmapi.lib" if="windows" />
+</target>
+')
+@:cppFileCode('
+ #include <dwmapi.h>
+ ')
+#if windows
+class WindowUtil
+{
+	@:functionCode('
+    int darkMode = enable ? 1 : 0;
+    HWND window = GetActiveWindow();
+    if (S_OK != DwmSetWindowAttribute(window, 19, &darkMode, sizeof(darkMode)))
+        DwmSetWindowAttribute(window, 20, &darkMode, sizeof(darkMode));
+    ')
+	public static function darkMode(enable:Bool)
+	{
+	}
 
-// 	public static function setTitle(?s:String, ?normal:Bool = true)
-// 	{
-// 		if (normal)
-// 			title(Application.current.meta.get('name') + " - " + s);
-// 		else if (s == null)
-// 			title(Application.current.meta.get('name'));
-// 		else
-// 			title(s);
-// 	}
+	public static function setTitle(?s:String, ?normal:Bool = true)
+	{
+		if (normal)
+			title(Application.current.meta.get('name') + " - " + s);
+		else if (s == null)
+			title(Application.current.meta.get('name'));
+		else
+			title(s);
+	}
 
-// 	// UHHHH
+	// UHHHH
 
-// 	static function title(string:String)
-// 		Application.current.window.title = string;
-// }
-// #end
+	static function title(string:String)
+		Application.current.window.title = string;
+}
+#end

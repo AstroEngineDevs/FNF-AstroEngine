2a09d399931447805b1611fb9c150790326334f0
HackDev <80620538+Hackx2@users.noreply.github.com>
2024-06-09 12:02:02 +0100

    Update TemplateVideo.hx

--- source/funkin/game/videos/TemplateVideo.hx
+++ source/funkin/game/videos/TemplateVideo.hx
@@ -1,5 +1,6 @@
 package funkin.game.videos;
 
+#if VIDEOS_ALLOWED
 class TemplateVideo extends MusicBeatState
 {
 	var video:MP4Handler;
@@ -11,8 +12,7 @@ class TemplateVideo extends MusicBeatState
 		FlxG.sound.music.fadeOut(1, 0, function(_) FlxG.sound.music.stop());
 		if (funkin.game.Main.fpsVar != null)
 			funkin.game.Main.fpsVar.visible = false;
-        FlxG.mouse.visible = false;
-
+		FlxG.mouse.visible = false;
 		video = new MP4Handler();
 		video.playVideo(Paths.video('VIDEOPATHEHERE')); // ripped from the game
 		video.finishCallback = function()
@@ -21,7 +21,7 @@ class TemplateVideo extends MusicBeatState
 				funkin.game.Main.fpsVar.visible = ClientPrefs.data.showFPS;
 			FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
 			FlxG.sound.music.fadeIn(4, 0, 0.7);
-            FlxG.mouse.visible = true;
+			FlxG.mouse.visible = true;
 			MusicBeatState.switchState(new MainMenuState());
 			return;
 		}
@@ -30,10 +30,10 @@ class TemplateVideo extends MusicBeatState
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
-
 		if ((FlxG.keys.justPressed.ANY && !controls.ACCEPT) && video != null)
 		{
 			video.finishVideo();
 		}
 	}
 }
+#end

571a2630ced8eaa250115d0200f695804631af41
HackDev <80620538+Hackx2@users.noreply.github.com>
2024-07-07 12:13:58 +0100

    update setup ofc

--- setup/setup-unix.sh
+++ setup/setup-unix.sh
@@ -3,15 +3,4 @@
 #
 # REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
 # https://haxe.org/download/version/4.2.5/
-haxelib install lime
-haxelib install openfl
-haxelib install flixel
-haxelib install flixel-addons
-haxelib install flixel-ui
-haxelib install flixel-tools
-haxelib install SScript
-haxelib install hxCodec
-haxelib install tjson
-haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxe -cp ./setup -D analyzer-optimize -main Main --interp
